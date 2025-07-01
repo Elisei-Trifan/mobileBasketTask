7904a9d765770ca105f01f6efb7520d1b8428c55
Elisei <slavaberzan@gmail.com>
2025-07-01 08:55:12 +0300

    Изменение автора коммитов

--- App.tsx
+++ App.tsx
@@ -75,7 +75,7 @@ const App: () => ReactNode = () => {
           style={{
             backgroundColor: isDarkMode ? Colors.black : Colors.white,
           }}>
-          <Section title="Step One">
+          <Section title="Step Two">
             Edit <Text style={styles.highlight}>App.js</Text> to change this
             screen and then come back to see your edits.
           </Section>
